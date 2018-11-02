3156a9e34985a3890724bf2bb1c1d319ea444b94
Katyanna Metheny <methenyk@dewv.edu>
2018-11-02 16:52:56 +0000

    Updates via sync script

--- puzzledPrincess.js
+++ puzzledPrincess.js
@@ -2,17 +2,38 @@ import { game, Sprite } from "./sgc/sgc.js";
 game.setBackground("floor.png");
 
 class Marker extends Sprite {
-    constructor(board, image, game) {
+    constructor(board, image, name) {
         super();
-        
+        this.board = board;
+        this.name = 'name';
+        this.setImage(image);
+        this.x = 150;
+        this.y = 275;
     }
 }
-class PrincessMarker extends Marker {}
+class PrincessMarker extends Marker {
+    constructor(board) {
+        super(board, "annFace.png", 'Princess Ann');
+        this.dragging = false;
+    }
+    handleMouseLeftButtonDown() {
+        this.dragging = true;
+    }
+    handleMouseLeftButtonUp() {
+        this.dragging = false;
+    }
+    handleGameLoop() {
+        if (this.dragging === true) {
+            this.x = game.getMouseX() - this.width / 2;
+            this.y = game.getMouseY() - this.height / 2;
+        }
+    }
+}
 class StrangerMarker extends Marker {}
 class TicTacToe extends Sprite {
     constructor() {
         super();
-        this.name = "A board";
+        this.name = 'A board';
         this.setImage("board.png");
         this.x = 300;
         this.y = 85;
@@ -20,7 +41,7 @@ class TicTacToe extends Sprite {
         this.size = 3;
         this.activeMarker; // variable exists, but value is undefined
     }
-    TakeTurns() {
+    takeTurns() {
         this.activeMarker = new PrincessMarker(this);
     }
 }
